51aba3381512867647771df14ad935389787b0a8
Khoale <khoalnapd08266@fpt.edu.vn>
2023-03-09 23:02:31 +0700

    up code

---
+++ lab1/app.js
@@ -0,0 +1,10 @@
+// Nhập vào 2 số nguyên a, b từ bàn phím
+var a = parseInt(prompt("Nhập số nguyên a: "));
+var b = parseInt(prompt("Nhập số nguyên b: "));
+
+// Tính tổng c của a và b
+var c = a + b;
+
+// In ra kết quả đồng thời bằng 2 cách
+document.write("Kết quả là: " + c);
+console.log("Kết quả là: " + c);
