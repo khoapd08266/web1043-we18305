f095d18142669fb0904d8368ee12984f96c6adfb
Khoale <khoalnapd08266@fpt.edu.vn>
2023-03-13 13:23:58 +0700

    up b4

--- lab2/b3.js
+++ lab2/b3.js
@@ -7,7 +7,7 @@ var tenRieng;
 //yeu can nhap ho ten va gioi tinh
 hoTen = prompt("Nhập vào họ và tên:");
 gioiTinh = prompt("Nhập vào giới tính (Nam/Nữ):");
-//tach ho ten va cac thanh phan 
+//tach ho va ten 
 var mangHoTen = hoTen.split(" ");
 ho = mangHoTen[0];
 tenRieng = mangHoTen[mangHoTen.length - 1];
@@ -19,12 +19,13 @@ if (mangHoTen.length > 2) {
 } else {
   tenDem = "";
 }
-//in ho ten dem ten rieng
+//in ho ten dem va ten rieng
 document.write("Họ: " + ho +"<br>");
 document.write("Tên đệm: " + tenDem +"<br>");
 document.write("Tên riêng: " + tenRieng +"<br>");
 //thay doi ten diem dua tren gioi tinh
-if (gioiTinh == "Nam") {
+if (gioiTinh == "Nam") 
+{
   tenDem = "Văn ";
 } else if (gioiTinh == "Nữ") {
   tenDem = "Vẽ";
